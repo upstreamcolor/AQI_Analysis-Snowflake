adccb722e2b24ff66b0af8ab909f62730e81e04a
upstreamolor <mayukhdas1999@gmail.com>
2025-12-21 00:50:41 +0530

    modified:   .github/workflows/data_ingest.yml 	modified:   ingest-api-data.py

--- ingest-api-data.py
+++ ingest-api-data.py
@@ -12,20 +12,6 @@ import logging
 # initiate logging at info level
 logging.basicConfig(stream = sys.stdout, level = logging.INFO, format = '%(levelname)s - %(message)s')
 
-# set the IST time zone
-ist_timezone = pytz.timezone('Asia/Kolkata')
-
-# get the current time in IST
-current_time_ist = datetime.now(ist_timezone)
-
-# format the timestamp
-timestamp = current_time_ist.strftime('%Y_%m_%d_%H_%M_%S')
-
-# create the file name
-file_name = f'air_quality_data_{timestamp}.json'
-
-today_string = current_time_ist.strftime('%Y_%m_%d')
-
 # authenticating to the Snowflake account
 def snowpark_basic_auth() -> Session:
     connection_parameters = {
@@ -64,42 +50,59 @@ def get_air_quality_data(api_key, limit):
         # check if the request was successful (status code 200)
         if response.status_code == 200:
 
-            logging.info('Received the data')
+            logging.info('Response received succesfully')
             
             # parse the JSON data from the response
             json_data = response.json()
 
-            logging.info('Writing the JSON file into local location before it moved to snowflake stage')
-            
-            # save the JSON data to a file
-            with open(file_name, 'w') as json_file:
-                json.dump(json_data, json_file, indent = 2)
+            # get the first occurence of `last_update`` from records [{}]
+            last_update_str = json_data.get("records", [{}])[0].get("last_update")
+            last_update = datetime.strptime(last_update_str, '%Y-%m-%dT%H:%M:%S.%f')
 
-            logging.info(f'File Written to local disk with name: {file_name}')
-            
+            # create the file name
+            file_name = f'air_quality_data_{last_update.strftime("%Y_%m_%d_%H_%M_%S")}.json'
+
+            # get today's date to create a directory with the same
+            today_string = last_update.strftime('%Y_%m_%d')
             stg_location = '@dev_db.stage_sch.raw_stg/india/'+ today_string +'/'
+
+            # authenticate to snowflake
             sf_session = snowpark_basic_auth()
             
-            logging.info('Snowflake login successful')
-
-            logging.info(f'Placing the file, {file_name} in stage location, {stg_location}')
-            sf_session.file.put(file_name, stg_location)
+            logging.info('Snowflake login successful...')
             
-            logging.info(f'JSON File placed successfully in stage location, {stg_location} in snowflake')
+            # list query to check existence of file to be uploaded in stage
             list_query = f'list {stg_location}{file_name}.gz'
             
+            # check for the existence of the file in stage
             logging.info(f'List query to fetch the stage file to check its existence = {list_query}')
             result_list = sf_session.sql(list_query).collect()
+
+            if result_list:
+                logging.info(f'File {file_name} already exists in stage {stg_location}, skipping upload.')
+            else:
+                logging.info('Writing the JSON file into local location before it is moved to snowflake stage')
+
+                # save the JSON data to a file
+                with open(file_name, 'w') as json_file:
+                    json.dump(json_data, json_file, indent = 2)
+                
+                logging.info(f'File Written to local disk with name: {file_name}')
+
+                # place the file from local to snowflake stage
+                logging.info(f'Placing the file, {file_name} in stage location, {stg_location}')
+                sf_session.file.put(file_name, stg_location)
+
+                logging.info(f'JSON File placed successfully in stage location, {stg_location} in snowflake')
             
-            logging.info(f'File is placed in snowflake stage location, {result_list}')
-            logging.info('The job completed successfully...')
+                logging.info('The job completed successfully...')
             
             # return the retrieved data
             return json_data
 
         else:
             # print an error message if the request was unsuccessful
-            logging.error(f"Error: {response.status_code} - {response.text}")
+            logging.error(f"{response.status_code} - {response.text}")
             sys.exit(1)
             #return None
 
